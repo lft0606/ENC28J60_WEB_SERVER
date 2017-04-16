01c0c7d0d5afe91045a9830183be3fa2ee5c5625
lft0606 <675428297@qq.com>
2017-04-16 20:40:49 +0800

    修改led.c的代码格式

--- USER/led.c
+++ USER/led.c
@@ -6,15 +6,15 @@ void LED_Init(void)
 	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA, ENABLE);
 	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOD, ENABLE);
 
-  GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_8;	
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;       
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_8;	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;       
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
 	
 	GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_2;	
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;       
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOD, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;       
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
 
 	GPIO_SetBits(GPIOA, GPIO_Pin_8);
 	GPIO_SetBits(GPIOD, GPIO_Pin_2);	
