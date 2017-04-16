db498f879f7cba5b2bff444a913e61a253241e61
lft0606 <675428297@qq.com>
2017-04-16 21:54:17 +0800

    修改gitignore文件，添加忽略的文件

--- USER/led.c
+++ USER/led.c
@@ -1,3 +1,15 @@
+/************************************************************
+Copyright (C), 1988-1999, Tech. Co., Ltd.
+FileName:   led.c
+Author  : 	LFT
+Version : 	V1.0
+Date    :		20170416
+Description: led初始化
+History: // 历史修改记录
+<author> <time> <desc>
+1、LFT 2017/04/16 创建该文件
+2、LFT 2017/04/17 修改LED的IO
+***********************************************************/
 #include "led.h"
 
 void LED_Init(void)
